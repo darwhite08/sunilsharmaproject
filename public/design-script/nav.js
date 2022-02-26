8d117e6a33f7a3e268c8e1f3ed35ad43d901275e
darwhite08 <chandrapriyanshu10@gmail.com>
2022-02-26 14:23:05 +0530

    8th commit

--- public/design-script/nav.js
+++ public/design-script/nav.js
@@ -1,4 +1,11 @@
-// function side_nav() {
-//     var x = document.getElementById("sb-user");
-//     x.style.display = 'inline-block';
-// }
+function side_nav() {
+    var x = document.getElementById("sb-user");
+    var y = document.getElementById("sb-nav");
+    x.style.display = 'block';
+    if (x.style.display === 'block') {
+        y.style.display = "block"
+    } else if (x.style.display === 'block' && y.style.display === "block") {
+        x.style.display = 'none';
+        y.style.display = 'none';
+    }
+}
