ef245e35773ff5e9a7ec980108d60345676c0284
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-03 14:00:44 +0530

    12th commit

---
+++ public/responsiveness-script/preview.js
@@ -0,0 +1,5 @@
+// for (let index = 0; index < document.querySelectorAll(".product-container").length; index++) {
+//     document.querySelectorAll(".product-container")[index].addEventListener("click", function() {
+//         document.querySelectorAll(".product-container")[index].setAttribute("href", "/")
+//     });
+// }
