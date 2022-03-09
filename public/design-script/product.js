f80991fdde4d5cba38390ea0382e21d2a74071e4
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-09 22:23:46 +0530

    24th commit

--- public/design-script/product.js
+++ public/design-script/product.js
@@ -1,8 +1,8 @@
 for (let i = 0; i < document.querySelectorAll('.product-container').length; i++) {
     if (window.innerWidth < 662) {
-        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:1%;")
-    } else if (window.innerWidth < 922 && window.innerWidth > 662) {
-        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:33%;")
+        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:100%;")
+    } else if (window.innerWidth < 922 || window.innerWidth > 662) {
+        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:100%;")
     } else {
         document.querySelectorAll('.product-container')[i].setAttribute("style", "width:22.5%;")
     }
