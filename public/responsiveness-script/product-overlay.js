0403ecde80e6432894bff716dbe75bdbe90ba573
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-09 09:29:42 +0530

    19th commit

--- public/responsiveness-script/product-overlay.js
+++ public/responsiveness-script/product-overlay.js
@@ -1,6 +1,19 @@
-// for (let i = 0; i < document.querySelectorAll(".product-container").lenght; i++) {
-//     document.querySelectorAll(".product-container")[i].addEventListener("click", function() {
+for (let i = 0; i < document.querySelectorAll(".product-container").lenght; i++) {
+    document.querySelectorAll(".product-container")[i].addEventListener("click", function() {
+        document.getElementById('po-main-id').style.display = "block";
 
-//     });
+    });
+}
+for (let index = 0; index < document.querySelectorAll(".product-container").length; index++) {
+    document.querySelectorAll(".product-container")[index].addEventListener("click", function() {
+        document.getElementById("po-main-id").style.display = 'block';
+        document.getElementById("po-main-cutter").addEventListener("click", function() {
+            document.getElementById("po-main-id").style.display = "none";
+        });
+        document.getElementById("pd-main-cutter").addEventListener("click", function() {
+            document.getElementById("po-main-id").style.display = "none";
+        });
 
-// }
+    });
+
+}
