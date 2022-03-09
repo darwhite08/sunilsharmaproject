a9a0632a5c7fb0c5e2b172d3cfecc350bdd62b6b
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-09 22:13:48 +0530

    23rd commit

--- file.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start hyper /k "git add . "
-echo git commit -m "next command"
-echo git push heroku master
-pause
-
-
--- public/design-script/nav.js
+++
@@ -1,8 +0,0 @@
-function side_nav() {
-    var x = document.getElementById("sb-user");
-    var y = document.getElementById("sb-nav");
-
-    if (x.style.display === 'block') {
-        y.style.display = 'block';
-    }
-}
