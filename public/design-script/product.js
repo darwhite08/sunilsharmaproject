523737e8eb82e3c95e7e69ee0db0d2d30cd5166c
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-09 22:31:38 +0530

    25th commit

--- public/design-script/product.js
+++ public/design-script/product.js
@@ -1,7 +1,7 @@
 for (let i = 0; i < document.querySelectorAll('.product-container').length; i++) {
     if (window.innerWidth < 662) {
         document.querySelectorAll('.product-container')[i].setAttribute("style", "width:100%;")
-    } else if (window.innerWidth < 922 || window.innerWidth > 662) {
+    } else if (window.innerWidth < 922) {
         document.querySelectorAll('.product-container')[i].setAttribute("style", "width:100%;")
     } else {
         document.querySelectorAll('.product-container')[i].setAttribute("style", "width:22.5%;")
