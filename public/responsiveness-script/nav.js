dbe0c183d7b606a861b35831f7d5ff33313aa7a0
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-10 15:04:13 +0530

    44

--- public/responsiveness-script/nav.js
+++ public/responsiveness-script/nav.js
@@ -1,5 +1,16 @@
-document.getElementById("tb-menu-slider").addEventListener("click", tb_menu_handler);
-
-function tb_menu_handler() {
-    document.getElementById("
+var clicked_times = 0;
+document.getElementById("tb-search-bar").addEventListener("click", function() {
+    document.getElementById("tb-div-search").style.cssText = `
+    display:block;
+    position:absolute;
+    top:100%;
+    left:0%;
+    width:100%;
+    background-color:white;
+    `;
+    clicked_times += 1;
+    if (clicked_times % 2 == 0) {
+        document.getElementById("tb-div-search").style.display = 'none';
     }
+    console.log(clicked_times);
+});
