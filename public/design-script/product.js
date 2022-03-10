f17cfbae086d7983e4e623fb1ea30ab014b2f761
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-10 13:18:39 +0530

    36

--- public/design-script/product.js
+++ public/design-script/product.js
@@ -1,9 +1,9 @@
 for (let i = 0; i < document.querySelectorAll('.product-container').length; i++) {
     if (window.innerWidth <= 662) {
-        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:40%;margin:0;");
+        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:40%; ");
     } else if (window.innerWidth <= 922 && window.innerWidth > 662) {
         document.querySelectorAll('.product-container')[i].setAttribute("style", "width:53%;");
     } else {
-        document.querySelectorAll('.product-container')[i].setAttribute("style", " width:22.5%;");
+        document.querySelectorAll('.product-container')[i].setAttribute("style", " width:22.5%; margin-right:2vw;");
     }
 }
