7043fa52cd1973b827337ac88f712774239cc1e1
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-10 12:28:55 +0530

    26th commit

--- public/design-script/product.js
+++ public/design-script/product.js
@@ -1,9 +1,9 @@
 for (let i = 0; i < document.querySelectorAll('.product-container').length; i++) {
-    if (window.innerWidth < 662) {
-        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:100%;")
-    } else if (window.innerWidth < 922) {
-        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:100%;")
+    if (window.innerWidth <= 662) {
+        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:45%;");
+    } else if (window.innerWidth <= 922 && window.innerWidth > 662) {
+        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:53%;");
     } else {
-        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:22.5%;")
+        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:22.5%;");
     }
 }
