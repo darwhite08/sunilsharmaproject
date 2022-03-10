ce338cf43e6f1100611fc7d98077d8afa720f659
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-10 13:23:12 +0530

    38

--- public/design-script/product.js
+++ public/design-script/product.js
@@ -1,6 +1,6 @@
 for (let i = 0; i < document.querySelectorAll('.product-container').length; i++) {
     if (window.innerWidth <= 662) {
-        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:40%; ");
+        document.querySelectorAll('.product-container')[i].setAttribute("style", "width:45%; ");
     } else if (window.innerWidth <= 922 && window.innerWidth > 662) {
         document.querySelectorAll('.product-container')[i].setAttribute("style", "width:53%;");
     } else {
