9205f3f29d2a7e7e236885d23b056e7096c3f5ef
darwhite08 <chandrapriyanshu10@gmail.com>
2022-03-10 22:35:48 +0530

    44

--- public/responsiveness-script/nav.js
+++ public/responsiveness-script/nav.js
@@ -1,5 +1,7 @@
-var clicked_times = 0;
-document.getElementById("tb-search-bar").addEventListener("click", function() {
+var clicked_times_tbs = 0;
+document.getElementById("tb-search-bar").addEventListener("click", tb_search_handler);
+
+function tb_search_handler() {
     document.getElementById("tb-div-search").style.cssText = `
     display:block;
     position:absolute;
@@ -8,9 +10,20 @@ document.getElementById("tb-search-bar").addEventListener("click", function() {
     width:100%;
     background-color:white;
     `;
-    clicked_times += 1;
-    if (clicked_times % 2 == 0) {
+    clicked_times_tbs += 1;
+    if (clicked_times_tbs % 2 == 0) {
         document.getElementById("tb-div-search").style.display = 'none';
     }
-    console.log(clicked_times);
-});
+}
+
+document.getElementById('tb-menu-slider').addEventListener("click", tb_ms_handler)
+
+function tb_ms_handler() {
+    document.getElementById("tb-menu").style.display = "block";
+
+}
+document.getElementById("tbm-cutter").addEventListener("click", tbmc_handler);
+
+function tbmc_handler() {
+    document.getElementById("tb-menu").style.display = "none";
+}
